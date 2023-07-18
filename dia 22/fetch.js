48802485443d314c022f99ac6f0570c41b27ea87
Alex <alex.conesa@yakuma.com>
2023-07-18 13:37:34 +0200

    API fetch completo

--- dia 22/fetch.js
+++ dia 22/fetch.js
@@ -1,11 +1,21 @@
-let usuarios;
+function leerUsuarios(mi_callback) {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((la_respuesta) => {
+            // Retorna un 'stream' al contenido
+            // Se recibira poco a poco hasta
+            // ejecutarse el siguiente then()
+            return la_respuesta.json();
+        }).then((todo_el_json) => {
+            // Ya tenemos toda la respuesta completa
+            // Llamamos al callback con el array recibido
+            mi_callback(todo_el_json)
+        });
+}
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((la_respuesta) => {
-        return la_respuesta.json();
-    })
-    .then((json) => usuarios = json);
+function listar_usuarios(array_usuarios) {
+    array_usuarios.forEach((user) => {
+        console.log(`Name: ${user.name}, Email: ${user.email}`);
+    });
+}
 
-// for (user of usuarios) {
-//     console.log(user);
-// }
+leerUsuarios(listar_usuarios);
